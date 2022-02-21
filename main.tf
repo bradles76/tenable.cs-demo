0d111710968fad42f0d6a58069fc727fdf31dc13
Adam Bradley <bradles76@hotmail.com>
2022-02-21 19:09:32 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
-resource "aws_instance" "app_server" {
-    ami           = "ami-05654370f5b5eb0b0"
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-05654370f5b5eb0b0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_group_ids = [aws_security_group.instance.id]
   
   user_data = <<-EOF
               #!/bin/bash
@@ -25,8 +25,8 @@ resource "aws_instance" "app_server" {
               nohup busybox httpd -f -p 8080 &
               EOF
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -37,5 +37,19 @@ resource "aws_security_group" "instance" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+  
+  min_size = 2
+  max_size = 10
+  
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
